94efd18e163fd0a6f966653c369be747d65a72f5
Samuel Lopes <samuelvitorprofissional@gmail.com>
2024-09-30 01:59:08 -0300

    🚀 Feat: Configurar rotas e NavBar no App.vue e ajustes no main.js
    Descrição:
    - Configurado `App.vue` para incluir o sistema de rotas e a barra de navegação (NavBar).
    - Atualizado `main.js` para configuração do Vue Router, Pinia, FontAwesome e Bulma.
    - Integradas dependências essenciais para o funcionamento do roteamento, gerenciamento de estado e estilização da aplicação.
    
    Número da Demanda: #22

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import '../node_modules/bulma/css/bulma.css';
+import App from './App.vue';
+import router from './app/Router/router';
+import './style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
