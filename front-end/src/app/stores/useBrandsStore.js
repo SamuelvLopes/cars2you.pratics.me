344b1a587b23324b48ab8993bd0c753028dfd61a
Samuel Lopes <samuelvitorprofissional@gmail.com>
2024-09-30 01:53:25 -0300

    🚀 Feat: Adicionar stores para brands, categories, colors, models e vehicles
    Descrição:
    - Criadas stores para gerenciamento de estado das entidades brands, categories, colors, models e vehicles.
    - Configurada lógica de manipulação de dados para otimizar o fluxo de informações na aplicação.
    
    Número da Demanda: #18

---
+++ front-end/src/app/stores/useBrandsStore.js
@@ -0,0 +1,20 @@
+// stores/useCategoriesStore.js
+import { defineStore } from 'pinia';
+import { getAllBrands } from '../services/brands';
+
+export const useBrandsStore = defineStore('brands', {
+  state: () => ({
+    brands: [],
+  }),
+
+  actions: {
+    async fetchBrands() {
+      try {
+        const data = await getAllBrands();
+        this.brands = data.data;
+      } catch (error) {
+        console.error('Erro ao buscar marcas:', error);
+      }
+    },
+  },
+});
